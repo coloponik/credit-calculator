c490a7fc75082225a03e1b3ca4daa3785526a665
Alice Ponikarovskaya <alisaponi@yandex.ru>
2019-11-30 18:49:38 +0500

    initial client validation

--- credit_calculator/Controllers/HomeController.cs
+++ credit_calculator/Controllers/HomeController.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using credit_calculator.Models;
 
 namespace credit_calculator.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
 
         public ActionResult CalculateCredit()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Result(Credit credit)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return RedirectToAction("CalculateCredit");
+        }
+
         public ActionResult ClearCredit()
         {
-            return RedirectToAction("Index");
+            return RedirectToAction("CalculateCredit");
         }
     }
 }
--- credit_calculator/Models/Credit.cs
+++ credit_calculator/Models/Credit.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace credit_calculator.Models
 {
     public class Credit
     {
+        [DisplayName("Credit amount:")]
+        [Required(ErrorMessage = "Field is required")]
+        [Range(1000, 3000000, ErrorMessage = "Invalid value")]
         public double CreditSum { get; set; }
+
+        [DisplayName("Credit term:")]
+        [Required(ErrorMessage = "Field is required")]
+        [Range(1, 1825, ErrorMessage = "Invalid value")]
         public double CreditPeriod { get; set; }
+
+        [DisplayName("Interest rate:")]
+        [Required(ErrorMessage = "field is required")]
+        [Range(1, 25, ErrorMessage ="Invalid value")]
         public double CreditRateYear { get; set; }
     }
 }
