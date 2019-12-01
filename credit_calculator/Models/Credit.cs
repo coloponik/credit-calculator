c53a5d5a3a2bd83d9a4fee8192416c1c8246c920
Alice Ponikarovskaya <alisaponi@yandex.ru>
2019-12-01 23:19:47 +0500

    completion client validation

--- credit_calculator/Models/Credit.cs
+++ credit_calculator/Models/Credit.cs
@@ -11,17 +11,20 @@ namespace credit_calculator.Models
     {
         [DisplayName("Credit amount:")]
         [Required(ErrorMessage = "Field is required")]
-        [Range(1000, 3000000, ErrorMessage = "Invalid value")]
+        [RegularExpression(@"\d+", ErrorMessage = "Must be positive number")]
+        [Range(20000, 250000, ErrorMessage = "Must be between 20 000 and 250 000")]
         public double CreditSum { get; set; }
 
-        [DisplayName("Credit term:")]
+        [DisplayName("Credit term (in months):")]
         [Required(ErrorMessage = "Field is required")]
-        [Range(1, 1825, ErrorMessage = "Invalid value")]
+        [RegularExpression(@"\d+", ErrorMessage = "Must be positive number")]
+        [Range(1, 36, ErrorMessage = "Must be between 1 and 36")]
         public double CreditPeriod { get; set; }
 
-        [DisplayName("Interest rate:")]
-        [Required(ErrorMessage = "field is required")]
-        [Range(1, 25, ErrorMessage ="Invalid value")]
+        [DisplayName("Interest rate (per year):")]
+        [Required(ErrorMessage = "Field is required")]
+        [RegularExpression(@"\d+([.]\d{1,2})?", ErrorMessage = "Must be positive number (limit to 2 decimal places)")]
+        [Range(10, 25, ErrorMessage ="Must be beetween 10 and 25")]
         public double CreditRateYear { get; set; }
     }
 }
