61ba14302e7bc139382fe8eb85b865e43deeac26
Alice Ponikarovskaya <alisaponi@yandex.ru>
2019-12-03 15:50:36 +0500

    completion server validation

---
+++ credit_calculator/Annotations/NotValidTerm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace credit_calculator.Annotations
+{
+    public class NotValidTerm : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                Regex regex = new Regex(@"\d+");
+                string str = value.ToString();
+                if (regex.IsMatch(str))
+                {
+                    double doubval = Convert.ToDouble(value);
+                    if ((doubval >= 1) && (doubval <= 36))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
