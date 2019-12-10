c8f4dfc7bbada879b6df251804841b3ba93126f1
Alice Ponikarovskaya <alisaponi@yandex.ru>
2019-12-10 17:23:07 +0500

    button-export

--- credit_calculator/Controllers/HomeController.cs
+++ credit_calculator/Controllers/HomeController.cs
@@ -9,16 +9,91 @@ using System.Web.Mvc;
 using System.Windows.Forms;
 using credit_calculator.Models;
 using Newtonsoft.Json;
+using Calabonga.Xml.Exports;
 
 namespace credit_calculator.Controllers 
 {
 
     public class HomeController : Controller 
     {
-
         public ActionResult CalculateCredit() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetCsvFile(Payment[] Model)
+        {
+            ExcelResult export = Export(Model);
+            return View("Result", export);
+        }
+
+        public ExcelResult Export(Payment[] dt)
+        {
+            string result = string.Empty;
+            Workbook wb = new Workbook();
+
+            wb.Properties.Author = "coloponik";
+            wb.Properties.Created = DateTime.Today;
+            wb.Properties.LastAutor = "coloponik";
+            wb.Properties.Version = "1";
+
+            // options sheets
+            wb.ExcelWorkbook.ActiveSheet = 1;
+            wb.ExcelWorkbook.DisplayInkNotes = false;
+            wb.ExcelWorkbook.FirstVisibleSheet = 1;
+            wb.ExcelWorkbook.ProtectStructure = false;
+            wb.ExcelWorkbook.WindowHeight = 800;
+            wb.ExcelWorkbook.WindowTopX = 0;
+            wb.ExcelWorkbook.WindowTopY = 0;
+            wb.ExcelWorkbook.WindowWidth = 600;
+
+            // create style s1 for header
+            Style s1 = new Style("s1");
+            s1.Font.Bold = true;
+            s1.Font.Italic = true;
+            s1.Font.Color = "#FF0000";
+            wb.AddStyle(s1);
+
+            // create style s2 for header
+            Style s2 = new Style("s2");
+            s2.Font.Bold = true;
+            s2.Font.Italic = true;
+            s2.Font.Size = 12;
+            s2.Borders.Add(new Border());
+            s2.Font.Color = "#0000FF";
+            wb.AddStyle(s2);
+
+            Worksheet ws3 = new Worksheet("Пользователи");
+
+            // Adding Headers
+            ws3.AddCell(0, 0, "Наименование (Name)");
+            ws3.AddCell(0, 1, "Возраст (Age)");
+            ws3.AddCell(0, 2, "Пол (Gender)");
+            ws3.AddCell(0, 3, "Вес (Weight)");
+            ws3.AddCell(0, 4, "Страна (Country)");
+
+            int totalRows = 0;
+
+            // appending rows with data
+            for (int i = 0; i < dt.Length; i++)
+            {
+                ws3.AddCell(i + 1, 0, dt[i].Id);
+                ws3.AddCell(i + 1, 1, dt[i].Date);
+                ws3.AddCell(i + 1, 2, dt[i].Body);
+                ws3.AddCell(i + 1, 3, dt[i].Percent);
+                ws3.AddCell(i + 1, 4, dt[i].MainBalance);
+                totalRows++;
+            }
+            totalRows++;
+
+            wb.AddWorksheet(ws3);
+
+            // generate xml 
+            string workstring = wb.ExportToXML();
+
+            // Send to user file
+            return new ExcelResult("payment_graphic.xls", workstring);
         }
 
         [HttpPost]
--- credit_calculator/Models/Credit.cs
+++ credit_calculator/Models/Credit.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
 using credit_calculator.Annotations;
+using Calabonga.Xml.Exports;
+using System.Text;
 
 namespace credit_calculator.Models
 {
@@ -93,5 +95,30 @@ namespace credit_calculator.Models
         public double MainBalance { get; set; }
         public double TotalOverPay { get; set; }
 
+    }
+
+    public class ExcelResult : ActionResult
+    {
+        public ExcelResult(string fileName, string report)
+        {
+            this.Filename = fileName;
+            this.Report = report;
+        }
+        public string Report { get; private set; }
+        public string Filename { get; private set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            HttpContext.Current.Response.Clear();
+            HttpContext.Current.Response.ContentType = "application/vnd.ms-excel";
+            HttpContext.Current.Response.BufferOutput = true;
+            HttpContext.Current.Response.AddHeader("content-disposition",
+                                 string.Format("attachment; filename={0}", Filename));
+            HttpContext.Current.Response.ContentEncoding = Encoding.UTF8;
+            HttpContext.Current.Response.Charset = "utf-8";
+            HttpContext.Current.Response.Write(Report);
+            HttpContext.Current.Response.Flush();
+            HttpContext.Current.Response.End();
+        }
     }
 }
